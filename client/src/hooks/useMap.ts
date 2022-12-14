258f8c14e856bfda8ea2b7194291b480338850de
marinahaugen <marina.santos.haugen@dnb.no>
2022-12-14 12:46:19 +0100

    need to fix hook and show feature

--- client/src/hooks/useMap.ts
+++ client/src/hooks/useMap.ts
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { Map } from "mapbox-gl";
 import { initMap } from "../utils/initMap";
 import { createFeatureHandler, getFeatureHandler } from "../services/features";
 
 export const useMap = async (container: React.RefObject<HTMLDivElement>) => {
   const mapInitRef = useRef<Map | null>(null);
-  const firstFeature = await getFeatureHandler('63986532d08583006e57b725');
+  const firstFeature = await getFeatureHandler("63986532d08583006e57b725");
 
   // if value of map container exists, initialize map
   useEffect(() => {
@@ -20,7 +20,8 @@ export const useMap = async (container: React.RefObject<HTMLDivElement>) => {
       mapInitRef.current.on("load", () => {
         console.log("Load done, ready to draw");
 
-        mapInitRef.current && mapInitRef.current.addSource("first", firstFeature);
+        mapInitRef.current &&
+          mapInitRef.current.addSource("first", firstFeature);
 
         mapInitRef.current &&
           mapInitRef.current.addLayer({
@@ -55,5 +56,5 @@ export const useMap = async (container: React.RefObject<HTMLDivElement>) => {
       mapInitRef.current.on("move", () => {
         console.log("UseMap: On the move");
       });
-  }, []);
+  }, [firstFeature]);
 };
